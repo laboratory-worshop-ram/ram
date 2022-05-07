18d452893c7e90d85b63582f73567b8c91284c25
dagalkova <dagalkova@miem.hse.ru>
2022-05-07 12:08:38 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const dropdownItem1 = document.querySelectorAll('.dropdown-item1');
     const dropdownItem2 = document.querySelectorAll('.dropdown-item2');
+    const dropdownItem3 = document.querySelectorAll('.dropdown-item3');
     const ramItems = document.querySelectorAll('.ram');
     const btnWarning = document.querySelector('.btn-warning');
+    const varItems = document.querySelectorAll('.vari');
 
     for (const item of dropdownItem1) {
         item.addEventListener('click', () => {
@@ -29,6 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    for (const item of dropdownItem3) {
+        item.addEventListener('click', () => {
+            dropdownItem3.forEach(item1 => {
+                if (item === item1) {
+                    item1.classList.add('active');
+                } else {
+                    item1.classList.remove('active');
+                }
+            })
+        })
+    }
+
+
     btnWarning.addEventListener('click', () => {
         for (const ram of ramItems) {
             ram.classList.add('hidden');
@@ -43,6 +58,17 @@ document.addEventListener('DOMContentLoaded', () => {
                             ram.classList.remove('hidden');
                         }
                     })
+                }
+            })
+        }
+
+        for (const vari of varItems) {
+            vari.classList.add('hidden');
+            dropdownItem3.forEach(item3 => {
+                if (!item3.classList.contains('active') && (vari.dataset.vari === item3.dataset.vari)) {
+                    vari.classList.add('hidden');
+                } else if (item3.classList.contains('active') && (vari.dataset.vari === item3.dataset.vari)) {
+                    vari.classList.remove('hidden');
                 }
             })
         }
